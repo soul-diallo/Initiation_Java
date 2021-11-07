6111a2a85c3011d0ebe9a7b5a2a3e10c3248dc53
@SoulOG <285963@supinfo.com>
2021-11-07 01:15:43 -0400

    creation tableau

--- src/tableau/Introduction.java
+++ src/tableau/Introduction.java
@@ -1,8 +1,26 @@
 package tableau;
 
+import java.util.Random;
+
 public class Introduction {
     public static void main(String[] args) {
         //Pour declarer un tableau
-        int [] entier = new int[10];
+//        int [] entier;
+//        entier = new int[]{12, 15, 4, 8, 66, 9, 88, 7, 4, 5,8};
+//        System.out.println("La taille du tableau est "+entier.length);
+//        for (int i =0;i< entier.length;i++){
+//            System.out.print(entier[i]+" ");
+//        }
+        int[] notes = new int[10];
+        double rand;
+        notes[0] = 52;
+        notes[1] = 40;
+        System.out.println(notes.length);
+        for (int i=0; i< notes.length;i++ ){
+            notes [i] = (int) (rand = Math.random()*100);
+        }
+        for (int i=0; i< notes.length;i++ ){
+            System.out.print(notes[i]+" ");
+        }
     }
 }
--- src/tableau/test.java
+++ src/tableau/test.java
@@ -1,7 +1,10 @@
 package tableau;
 
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("It's work");
+        Scanner clavier = new Scanner(System.in);
+
     }
 }
