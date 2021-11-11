590e8b562e6380fe77320d868e3f93ad28ad231c
@SoulOG <285963@supinfo.com>
2021-11-11 17:34:06 -0500

    Debut des fonctions

---
+++ src/tableau/Fonctions.java
@@ -0,0 +1,7 @@
+package tableau;
+
+public class Fonctions {
+    public static void main(String[] args) {
+
+    }
+}
--- src/tableau/Tableau_dynamique_and_string.java
+++ src/tableau/Tableau_dynamique_and_string.java
@@ -1,7 +1,21 @@
 package tableau;
 
+import java.util.ArrayList;
+
 public class Tableau_dynamique_and_string {
     public static void main(String[] args) {
+//        //Declaration et initialisation d'un string
+//        String unNom;
+//        String message = "Gang";
+//        System.out.print(message.charAt(0));
+
+        //Decalration d'un tableau dynamique
+
+        ArrayList<String> tableau = new ArrayList<String>();
+        System.out.println(tableau.size());
+        tableau.add(0,"Gang");
+        System.out.println(tableau.get(0));
+        tableau.add("Souai");
 
     }
 }
