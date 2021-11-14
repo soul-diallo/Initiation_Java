7cf2d48eb71435983d831c2b430689e54660588a
@SoulOG <285963@supinfo.com>
2021-11-14 01:44:32 -0500

    Fin des cours videos

--- src/tableau/Fonctions.java
+++ src/tableau/Fonctions.java
@@ -1,8 +1,20 @@
 package tableau;
 
+import java.util.Scanner;
+
 public class Fonctions {
     public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
         int t = 51;
         System.out.println(t);
+        System.out.println("Veuillez saisir votre nom: ");
+        String name = clavier.next();
+        System.out.println("Votre nom est "+ afficheNom(name));
     }
+    //Déclaration d'une fonction
+    static String afficheNom (String name){
+
+        return name;
+    }
+
 }
