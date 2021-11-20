c3a7cda1ca06b0d24948a1f14115aedbf4a21e36
@SoulOG <285963@supinfo.com>
2021-11-20 18:31:07 -0500

    Implementation de la fonction initialise et affiche

--- src/projet_puissance_4/puissance_4.java
+++ src/projet_puissance_4/puissance_4.java
@@ -1,10 +1,49 @@
 package projet_puissance_4;
 
 public class puissance_4 {
+    // Declaration des constantes
+    private final static int VIDE =0;
+    private final static int JAUNE = 1;
+    private final static int ROUGE = 2;
+
     public static void main(String[] args) {
-        // TODO: Declaration des variables
-        int tab[][] = {};
-        // TODO: Afficher le tableau de jeu
-        // TODO: Moteur du jeu
+        int [][] grille = new int[6][7];
+        initialise(grille);
+        grille[2][3] = JAUNE;
+        grille[2][4] = ROUGE;
+        affiche(grille);
+    }
+
+    static void initialise(int [][] grille){
+        for (int i = 0; i< grille.length;i++){
+            for (int j =0;j<grille[i].length;j++){
+                grille[i][j] = VIDE;
+            }
+        }
+    }
+
+    //affiche O pour une case rouge, X pour une case jaune
+    static void affiche(int[][] grille){
+        for (int[] ligne : grille){
+            System.out.print(" |");
+            for (int cellule :ligne){
+                if (cellule == VIDE){
+                    System.out.print(' ');
+                }
+                else if (cellule == ROUGE){
+                    System.out.print('O');
+                }
+                else {
+                    System.out.print('X');
+                }
+                System.out.print('|');
+            }
+            System.out.println();
+        }
+        System.out.print('=');
+        for (int i = 1; i<= grille[0].length; ++i){
+            System.out.print("=" + i);
+        }
+        System.out.println("==\n");
     }
 }
